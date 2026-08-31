578e3f069623e5d5045ff7540609d0f78e4c743b
Michael Trimarchi <michael@amarulasolutions.com>
2026-08-31 10:29:54 +0200

    SoC: bcm: giotto: Add hardware volume using iio subsystem
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- sound/soc/bcm/giotto.c
+++ sound/soc/bcm/giotto.c
@@ -15,6 +15,8 @@
 #include <linux/clk.h>
 #include <linux/gpio.h>
 #include <linux/delay.h>
+#include <linux/iio/iio.h>
+#include <linux/iio/consumer.h>
 #include <sound/soc.h>
 #include <sound/pcm_params.h>
 
@@ -28,6 +30,10 @@
 
 struct giotto_data {
 	struct snd_soc_card card;
+
+	/* IIO */
+	struct iio_channel *iio_ch;
+	int volume;
 };
 
 static const struct snd_soc_dapm_widget giotto_dapm_widgets[] = {
@@ -116,6 +122,62 @@ static int giotto_dai_init(struct snd_soc_pcm_runtime *rtd)
 	return 0;
 }
 
+static int volume_giotto_ctl_info(struct snd_kcontrol *kcontrol,
+				  struct snd_ctl_elem_info *uinfo)
+{
+	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
+	uinfo->count = 1;
+	uinfo->value.integer.min = 37;
+	uinfo->value.integer.max = 100;
+	return 0;
+}
+
+static int volume_giotto_ctl_get(struct snd_kcontrol *kcontrol,
+				 struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_card *card = snd_kcontrol_chip(kcontrol);
+	struct giotto_data *data = snd_soc_card_get_drvdata(card);
+
+	ucontrol->value.integer.value[0] = data->volume;
+
+	return 0;
+}
+
+static int volume_giotto_ctl_put(struct snd_kcontrol *kcontrol,
+				 struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_card *card = snd_kcontrol_chip(kcontrol);
+	struct giotto_data *data = snd_soc_card_get_drvdata(card);
+	int val;
+	int ret;
+
+	val = (int)ucontrol->value.integer.value[0] - 100;
+
+	ret = iio_write_channel_attribute(&data->iio_ch[0], val, 0,
+					  IIO_CHAN_INFO_HARDWAREGAIN);
+	if (ret < 0)
+		return ret;
+
+	ret = iio_write_channel_attribute(&data->iio_ch[1], val, 0,
+					  IIO_CHAN_INFO_HARDWAREGAIN);
+	if (ret < 0)
+		return ret;
+
+	data->volume = ucontrol->value.integer.value[0];
+
+	return 0;
+}
+
+static struct snd_kcontrol_new giotto_ctl[] = {
+	{
+		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
+		.name = "PCM Playback Volume",
+		.info = volume_giotto_ctl_info,
+		.get = volume_giotto_ctl_get,
+		.put = volume_giotto_ctl_put,
+	},
+};
+
 SND_SOC_DAILINK_DEFS(giotto,
 	DAILINK_COMP_ARRAY(COMP_CPU("bcm2835-i2s.0")),
 	DAILINK_COMP_ARRAY(COMP_CODEC("spi0.0", "pcm179x-hifi")),
@@ -170,11 +232,21 @@ static int giotto_probe(struct platform_device *pdev)
 	data->card.num_links = 1;
 	data->card.dapm_widgets = giotto_dapm_widgets;
 	data->card.num_dapm_widgets = ARRAY_SIZE(giotto_dapm_widgets);
+	data->card.controls = giotto_ctl;
+	data->card.num_controls = ARRAY_SIZE(giotto_ctl);
+	snd_soc_card_set_drvdata(&data->card, data);
 
 	giotto_dai.cpus->dai_name = NULL;
 	giotto_dai.cpus->of_node = i2s_np;
 	giotto_dai.platforms->name = NULL;
 	giotto_dai.platforms->of_node = i2s_np;
+
+	data->iio_ch = devm_iio_channel_get_all(dev);
+	if (IS_ERR(data->iio_ch)) {
+		dev_err(dev, "snd_soc_register_card iio_ch failed\n");
+		ret = -EPROBE_DEFER;
+		goto fail;
+	}
 
 	ret = devm_snd_soc_register_card(dev, &data->card);
 	if (ret) {
